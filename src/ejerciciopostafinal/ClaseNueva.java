e4f3e771179eef8e91032a496cbcd398856dbe11
tejaART <rodri,teja13.com>
2019-06-17 01:42:44 -0300

    agrego clase y pongo amarillo en el main

--- src/ejerciciopostafinal/ClaseNueva.java
+++ src/ejerciciopostafinal/ClaseNueva.java
@@ -9,13 +9,6 @@ package ejerciciopostafinal;
  *
  * @author TejaART
  */
-public class EjercicioPostaFinal {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("amarillo");
-    }
+public class ClaseNueva {
     
 }
